a9ba0a2c24edb166e2d0871f70a536abf349fa2b
rodrigosa7 <digex2008@live.com.pt>
2021-05-24 17:35:26 +0100

    First Commit
    Endpoint login
    Endpoint Create user

---
+++ index.js
@@ -0,0 +1,83 @@
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const hostname = "127.0.0.1";
+const port = 3000;
+const saltRounds = 10;
+
+app.listen(port, () => {
+  console.log(`runnig server! http://${hostname}:${port}/`);
+});
+
+//Olá mundo
+app.get("/", (req, res) => {
+  res.send("Olá");
+  console.log("Olá mundo");
+});
+
+//--------------------------------- Login ---------------------------
+
+//Login
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  db.query("SELECT * FROM users WHERE email = ?", email, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    if (result.length > 0) {
+      bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+          console.log(err);
+        }
+
+        bcrypt.compare(password, result[0].password, (error, response) => {
+          if (response) {
+            res.json({ status: true, email: email });
+          } else {
+            res.json({ status: false });
+          }
+        });
+      });
+    }
+  });
+});
+
+// ---------------------------- Utilizadores ----------------------------------------------------
+
+//Adicionar user
+app.post("/user", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  db.query("SELECT * FROM `users` WHERE email = ?", email, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+
+    if (result.length > 0) {
+      res.send("Já existe um utilizador com esse email");
+    } else {
+      bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+          res.send(err);
+        }
+        db.query(
+          "INSERT INTO users (email, password) VALUES (?, ?)",
+          [email, hash],
+          (err, result) => {
+            if (err) {
+              res.send(err);
+            } else {
+              res.send("Registo com sucesso");
+            }
+          }
+        );
+      });
+    }
+  });
+});
